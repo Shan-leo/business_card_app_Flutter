b2eb97ef2e212052b157c9402935be17546a1355
Shan-leo <bizwithshan@gmail.com>
2022-06-02 21:43:10 +0530

    version 1.0.0

--- lib/main.dart
+++ lib/main.dart
@@ -11,28 +11,64 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
+        backgroundColor: Colors.blue.shade800,
         body: SafeArea(
           child: Column(
-            children:  [
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
               const CircleAvatar(
                 radius: 50,
                 backgroundImage: AssetImage('images/dp.jpg'),
               ),
-              const Text('Shan Leo',
+              const Text(
+                'Shan Leo',
                 style: TextStyle(
-                    fontSize: 40,
-                    color: Colors.white,
-                    fontFamily: 'Pacifico'
-                ),),
-              Text('SOFTWARE ENGINEER',
-                style:TextStyle(
-                  fontSize: 15,
-                  color: Colors.teal.shade100,
-                  letterSpacing: 2.5,
-                  fontWeight: FontWeight.bold,
-                  fontFamily: 'Source Sans Pro'
-                ),)
+                    fontSize: 40, color: Colors.white, fontFamily: 'Pacifico'),
+              ),
+              Text(
+                'SOFTWARE ENGINEER',
+                style: TextStyle(
+                    fontSize: 15,
+                    color: Colors.teal.shade100,
+                    letterSpacing: 2.5,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Source Sans Pro'),
+              ),
+              SizedBox(height: 15,
+              width: 150,
+              child: Divider(
+                color: Colors.teal.shade100,
+              ),),
+              Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      '+946754575',
+                      style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source Sans Pro'),
+                    ),
+                  )),
+              Card(
+                  color: Colors.white,
+                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                  child: ListTile(
+                    leading: Icon(
+                      Icons.email,
+                      color: Colors.teal,
+                    ),
+                    title: Text(
+                      'shanleo@shan.com',
+                      style: TextStyle(
+                          color: Colors.teal.shade900,
+                          fontFamily: 'Source Sans Pro'),
+                    ),
+                  ))
             ],
           ),
         ),
