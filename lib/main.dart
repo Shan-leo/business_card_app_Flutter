776dfc6f0425a1fcf1fab0f9ee62b7729dcfa56b
Shan-leo <bizwithshan@gmail.com>
2022-06-02 06:11:30 +0530

    Avatar added

--- lib/main.dart
+++ lib/main.dart
@@ -13,14 +13,30 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            color: Colors.white,
-            child: Text('Hello'),
+          child: Column(
+            children:  [
+              const CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/dp.jpg'),
+              ),
+              const Text('Shan Leo',
+                style: TextStyle(
+                    fontSize: 40,
+                    color: Colors.white,
+                    fontFamily: 'Pacifico'
+                ),),
+              Text('SOFTWARE ENGINEER',
+                style:TextStyle(
+                  fontSize: 15,
+                  color: Colors.teal.shade100,
+                  letterSpacing: 2.5,
+                  fontWeight: FontWeight.bold,
+                  fontFamily: 'Source Sans Pro'
+                ),)
+            ],
           ),
         ),
       ),
     );
   }
-
-
 }
